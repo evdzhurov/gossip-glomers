e10d385e3d447392a8ed1ba734c36dc7eaa68127
Evgeniy Dzhurov <edj@fastmail.fm>
2025-10-31 10:43:33 +0200

    Add skeleton for broadcast single node test case

---
+++ cmd/broadcast/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		reply := map[string]any{
+			"type": "broadcast_ok",
+		}
+
+		return n.Reply(msg, reply)
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "read_ok"
+
+		msgs := []any{}
+		body["messages"] = msgs
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		reply := map[string]any{
+			"type": "topology_ok",
+		}
+
+		return n.Reply(msg, reply)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
